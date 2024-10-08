0335fde5d41e1f540b4ce7f14dc7ec7a7b5744aa
Babs <68171102+Ohubabs@users.noreply.github.com>
2024-10-08 12:28:28 -0700

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,9 @@
+variable "frieza" {
+    description = "Username for Elastic/Kibana Dashboard"
+    default = "elastic" 
+}
+
+variable "deathbeam" {
+    description = "Password for Elastic/Kibana Dashboard"
+    default = "" 
+}
