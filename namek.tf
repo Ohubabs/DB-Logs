833c68d4c1314689db68cc674f219faa6e98d924
Babs <68171102+Ohubabs@users.noreply.github.com>
2024-10-08 12:27:26 -0700

    Create namek.tf

---
+++ namek.tf
@@ -0,0 +1,13 @@
+/*resource "kubernetes_secret_v1" "elastic-auth" {
+  metadata {
+    name = "namek"
+    namespace = "monitor"
+  }
+
+  data = {
+    username = var.frieza
+    password = var.deathbeam
+  }
+
+  type = "Opaque"
+}*/
